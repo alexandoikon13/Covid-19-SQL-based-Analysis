dd499240cf591b644960f775032f74a3c77a6f56
alexandoikon13 <alexandoikon@gmail.com>
2023-11-15 20:46:55 +0100

    created script to copy data from CSV to Table

---
+++ scripts/import_data.sql
@@ -0,0 +1,4 @@
+COPY (FIPS, Admin2, Province_State, Country_Region, Last_Update, Lat, Long_, Confirmed, Deaths, Recovered, Active, Combined_Key, Incident_Rate, Case_Fatality_Ratio)
+FROM './data/11-12-2022.csv'
+DELIMITER ','
+CSV HEADER;
