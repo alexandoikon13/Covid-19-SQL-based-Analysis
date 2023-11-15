f67b33fe5a17ec4c0a71b1c2bbd081b9011caeb2
alexandoikon13 <alexandoikon@gmail.com>
2023-11-15 21:44:11 +0100

    merged code into 1 file & created preprocess file

--- scripts/import_data.sql
+++ scripts/import_data.sql
@@ -1,4 +1,29 @@
-COPY covid_data(FIPS, Admin2, Province_State, Country_Region, Last_Update, Lat, Long_, Confirmed, Deaths, Recovered, Active, Combined_Key, Incident_Rate, Case_Fatality_Ratio)
-FROM './data/11-12-2022.csv'
-DELIMITER ','
-CSV HEADER;
+CREATE DATABASE covid_analysis;
+
+CREATE TABLE covid_data (
+    FIPS INT,
+    Admin2 VARCHAR(50),
+    Province_State VARCHAR(50),
+    Country_Region VARCHAR(50),
+    Last_Update DATE,
+    Lat NUMERIC,
+    Long_ NUMERIC,
+    Confirmed INTEGER,
+    Deaths INTEGER,
+    Recovered INTEGER,
+    Active INTEGER,
+    Combined_Key VARCHAR(100),
+    Incident_Rate NUMERIC,
+    Case_Fatality_Ratio NUMERIC
+);
+
+\copy covid_data from 'C:/Users/alexa/Dropbox/My PC (LAPTOP-TALO5C9C)/Desktop/Study Challenge/myportfolio/Projects_VSCode/Covid-19-SQL-based-Analysis/data/csse_covid_19_data/csse_covid_19_daily_reports/11-12-2022.csv' DELIMITER ',' CSV HEADER;
+
+ALTER TABLE covid_data
+DROP COLUMN FIPS,
+DROP COLUMN Admin2,
+DROP COLUMN Province_State,
+DROP COLUMN Lat,
+DROP COLUMN Long_,
+DROP COLUMN Combined_Key
+;
